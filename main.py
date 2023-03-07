b8022d8a230989069217ccf08358fde750d69664
Amber Benbow <anbenbow@gmail.comgit config --global user.email anbenbow@gmail.com>
2023-03-06 19:26:29 -0500

    created patient profiles and treatment list

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ import zulu
 import random
 
 
+male_treatment= "EVMY"
+female_treatment= "EVFX"
 
+treatments=['Blood Work','X-Ray','MRI','Doppler','Ultrasound','Suture']
 
-
-
-
-treatment_dictionary={
-[]
-
-
-
-
-
-
-
+Patient_0001={
+'Name': 'Jon Snow',
+'Gender': 'M'
+}
+Patient_0002={
+'Name': 'Bran Stark',
+'Gender': 'M'
+}
+Patient_0003={
+'Name': 'Daenerys Targaryen',
+'Gender': 'F'
+}
+Patient_0004={
+'Name': 'Cersei Lannister',
+'Gender': 'F'
+}
+Patient_0005={
+'Name': 'Joffrey Baratheon',
+'Gender': 'M'
 }
