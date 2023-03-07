b6e1c71ffa213a45a181be5271636378753605d2
Amber Benbow <anbenbow@gmail.comgit config --global user.email anbenbow@gmail.com>
2023-03-07 18:48:05 -0500

    setting intake time and initial eval timeframe using zulu

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import random
 
 male_treatment= "EVMY"
 female_treatment= "EVFX"
+re_eval= "REEV"
 
 treatments=['Blood Work','X-Ray','MRI','Doppler','Ultrasound','Suture']
+
+start= zulu.parse('2023-03-07T23:33:25.656261+00:00')
+initial_eval= start.shift(minutes=15)
+#print(start)
+print(initial_eval-start)
 
 Patient_0001={
 'Name': 'Jon Snow',
@@ -27,3 +33,12 @@ Patient_0005={
 'Name': 'Joffrey Baratheon',
 'Gender': 'M'
 }
+# print(Patient_0001['Name'])
+# print(Patient_0001['Gender'])
+
+
+# end= zulu.now() 
+
+# total_time= end-start
+# print(total_time)
+# print(end)
